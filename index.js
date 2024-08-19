3eeffafb9e9dd5a20a4be95b5db1cb687873937d
Josh <RimesJ@cardiff.ac.uk>
2024-08-19 19:45:14 +0100

    added 3 initial dropdown buttons in the tool bar (non-functional)

---
+++ index.js
@@ -0,0 +1,27 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function colourButton() {
+    document.getElementById("colourButton").classList.toggle("show");
+}
+
+function brushSizeButton() {
+    document.getElementById("brushSizeButton").classList.toggle("show");
+}
+
+function brushTypeButton() {
+    document.getElementById("brushTypeButton").classList.toggle("show");
+}
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.toolBar')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
