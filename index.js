f44ba6c532ff9bdf752dbfc04a3784aba754eeca
Josh Rimes - Laptop <joshjohn1104@gmail.com>
2024-10-01 16:01:35 +0100

    changed names of classes for clearer understanding

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ function clearButton() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clearRect creates a rect of transparent colour
 }
 
-// FIXME Buttons aren't in one row
-
 // When the user clicks on these buttons, toggle between hiding and showing the dropdown content 
 function colourButton() {
     document.getElementById("colourButton").classList.toggle("show");
@@ -49,8 +47,8 @@ function eraserButton() {
 // DROPDOWN FUNCTIONS
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.toolBarSection') && !event.target.matches('.buttonClass-content')) {
-        var dropdowns = document.getElementsByClassName("buttonClass-content");
+    if (!event.target.matches('.toolBarButton') && !event.target.matches('.dropdown-content')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
